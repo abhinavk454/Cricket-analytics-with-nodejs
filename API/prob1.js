5019652d4817e245c130add6729cd998ff9bc77e
Abhinav <abhinav.vikash2@gmail.com>
2021-12-24 01:32:59 +0530

    Correct the 2017 issue.

--- API/prob1.js
+++ API/prob1.js
@@ -4,19 +4,20 @@ const Delivery = require("../db/models/deliveries");
 
 var countObj = {};
 var ids = 0;
+var idl = 0;
 module.exports = function (req, res) {
   fs.createReadStream("./matches.csv")
     .pipe(csv())
     .on("data", (data) => {
       if (data["SEASON"] == "2016" && data["TOSS_DECISION"] == "field") {
-        while (ids < 11) {
+        if (ids < 11) {
           countObj["2016"] = { ...countObj["2016"], [ids]: data };
           ids = ids + 1;
         }
       } else if (data["SEASON"] == "2017" && data["TOSS_DECISION"] == "field") {
-        while (ids < 11) {
-          countObj["2017"] = { ...countObj["2017"], [ids]: data };
-          ids = ids + 1;
+        if (idl < 11) {
+          countObj["2017"] = { ...countObj["2017"], [idl]: data };
+          idl = idl + 1;
         }
       }
     })
